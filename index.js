c641a53ee555a7d2a812c908d1f22e78af3f2ca9
Arnab Mac <arnab.kundu1990@gmail.com>
2023-11-14 05:14:03 +0530

    Screenshots

--- index.js
+++ index.js
@@ -4,8 +4,18 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+
 import Charts from './pages/Charts';
+//import GetApiCall from './pages/networking/GetApiCall';
+//import PostApiCall from './pages/networking/PostApiCall';
+
+import SampleLoginScreen from './screens/SampleLoginScreen';
+
 import {name as appName} from './app.json';
 
 //AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerComponent(appName, () => Charts);
+//AppRegistry.registerComponent(appName, () => GetApiCall);
+//AppRegistry.registerComponent(appName, () => PostApiCall);
+
+AppRegistry.registerComponent(appName, () => SampleLoginScreen);
